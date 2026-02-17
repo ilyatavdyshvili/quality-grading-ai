c459f047d52f842e06ae8a3ce41b209747f0a017
ilyatavdyshvili <wo.ilya.tavdyshvili.rk@gmail.com>
2026-02-17 16:21:11 +0400

    Create services.py

---
+++ src/application/services.py
@@ -0,0 +1,15 @@
+from src.domain.models import ProductSample, QualityGrade
+from src.domain.interfaces import IQualityClassifier
+
+class QualityControlService:
+    def __init__(self, classifier: IQualityClassifier):
+        self.classifier = classifier
+
+    def evaluate_sample(self, sample: ProductSample) -> QualityGrade:
+        # Масштабирование признаков (Min-Max 0..1)
+        scaled_sample = ProductSample(
+            acidity=sample.acidity / 10,
+            density=sample.density / 1.1,
+            alcohol_content=sample.alcohol_content / 16
+        )
+        return self.classifier.evaluate(scaled_sample)
