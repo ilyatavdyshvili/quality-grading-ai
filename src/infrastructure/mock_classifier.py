713af7958a9c64e71db20d745ca8936a39d0a7f9
ilyatavdyshvili <wo.ilya.tavdyshvili.rk@gmail.com>
2026-02-17 16:54:48 +0400

    Delete src/infrastructure/mock_classifier.py

--- src/infrastructure/mock_classifier.py
+++
@@ -1,9 +0,0 @@
-from src.domain.interfaces import IQualityClassifier
-from src.domain.entities import ProductSample, QualityGrade
-
-class FormulaClassifier(IQualityClassifier):
-    def evaluate(self, sample: ProductSample) -> QualityGrade:
-        # Взвешенная формула оценки качества
-        score = (sample.acidity * 0.3 + sample.density * 0.3 + sample.alcohol_content * 0.4) / 10
-        label = "Premium" if score > 0.7 else "Standard"
-        return QualityGrade(score=score, label=label)
